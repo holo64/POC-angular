f161487f184ee7832c2dd7d778950647686c3ce7
Pascale Tran <ptran01.pro@gmail.com>
2020-07-23 19:17:01 +0200

    app avec APIs Clients et Frais

--- src/app/frais/frais.ts
+++ src/app/frais/frais.ts
@@ -1,21 +1,21 @@
 export class Frais {
 
     id: number;                     //son id
-    type_frais: Array<string>;      //types de frais(restaurant, hotel..)
-    date_paiement: Date             //Date de Paiement
+    typeFrais: string;      //types de frais(restaurant, hotel..)
+    datePaiement: Date             //Date de Paiement
     description: string;            //Description
     montant: number;                //Montant
-    type_paiement: string;          //types de paiement(espèces, chèque, CB..)
+    typePaiement: string;          //types de paiement(espèces, chèque, CB..)
     justificatif: string;           //url de son image
-    created_at: Date;               //date de création
-    created_by: string;             //Création par
-    last_updated_at: Date;          //Date Modification
-    last_updated_by: string;        //Modifié par
+    createdAt: Date;               //date de création
+    createdBy: string;             //Création par
+    lastUpdatedAt: Date;          //Date Modification
+    lastUpdatedBy: string;        //Modifié par
 
     //initialisations par défaut
     constructor() { 
       this.id=0;
-      this.created_at = new Date();
-      this.type_frais = [];
+      this.createdAt = new Date();
+      //this.typeFrais = [];
     }
 }
